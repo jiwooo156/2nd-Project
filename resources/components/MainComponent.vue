d2f08ddce8aeade06a801ebfcbf312f1b34f7f85
mingjicha <mingi9100@naver.com>
2023-12-14 19:13:56 +0900

    1214 main_2 작업 중

--- resources/components/MainComponent.vue
+++ resources/components/MainComponent.vue
@@ -2,10 +2,9 @@
 	<div>
 		<div class="main">
 			<div class="main_1">
-				<!-- <img class="main_sl" src="./img/maru.png" alt="메인이미지" /> -->
 				<div class="main_1_box">
           <div class="main_txt">
-            <div class="main_txt_up">
+            <div class="main_txt_up animate__animated animate__lightSpeedInRight">
               놀러오세요! 경상도, 좋아요!<br />
             </div>
             <div class="main_txt_down">
@@ -20,45 +19,53 @@
 					</div>
 				</div>
 			</div>
-			<!-- 왼쪽 축제정보-- 이거 한 묶음, 왼쪽 색연필 + 경상도!랑 #한 묶음, 보도뉴스들 한 묶음 총 세개로 플렉스 넣기   -->
 			<div class="main_2">
         <div class="main_2_box">
-          <div class="main_2_box_left"
+          <div class="main_box_left"
               data-aos="fade-down"
               data-aos-easing="linear"
               data-aos-duration="1300">
-            <div class="main_2_sub">
-              <div class="main_2_sub_tit">축제정보</div>
-              <div class="main_2_sub_line"></div>
+            <div class="main_sub">
+              <div class="main_sub_tit">축제정보</div>
+              <div class="main_sub_line"></div>
             </div>
-            <div class="main_2_sub_con">
-              <div class="main_2_sub_txt">
+            <div class="main_sub_con">
+              <div class="main_sub_txt">
                 경상도에서 인기있는<br />
                 축제를 만나보세요!
               </div>
             </div>
             <div class="main_shap">크리스마스 우리 함께 즐겨볼까요🎅</div>
-            <!-- 반응형 사라질 때 노란효과 없애기 -->
-            <img class="main_sub_y" src="/img/yellow.png" alt="색연필" />
+            <!-- 반응형 사라질 때 노란색연필 없애기 -->
+            <img class="main_sub_y" src="/img/yellow.png" alt="pencil" />
           </div>
-          <div class="main_2_box_right">
-            <!-- <div class="main_2_news">뉴스</div> -->
+          <div class="main_box_right">
+            <div class="main_2_news">
+              <div class="main_2_news_l">
+                <div class="art_plus">
+                  <h3 class="article">보도자료</h3>
+                  <div class="plus_icon"><font-awesome-icon :icon="['fas', 'plus']" /></div>
+                </div>
+                <h3 class="art_tit">내년에도 지역 체류여행은 계속된다. 군내ㅇ행 기상도 'ㅁ락믐'</h3>
+                <p class="art_con">내년에도 지역 체류여행은 계속된다. 국내여행 기상도 '맑음'...</p>
+                <p class="art_date">2023-12-12</p>
+              </div>
+              <div class="main_2_news_l">dd</div>
+              <div class="main_2_news_l">d</div>
+              <div class="main_2_news_l">d</div>
+            </div>
           </div>
-          <!-- <div class="main_2_news"> -->
-            <!-- 네모 박스가 세로로 줄어들 때 자꾸 줄어들어억!! -->
-            <!-- 뉴스 가져오기 a링크 -->
-          <!-- </div> -->
 				</div>
 			</div>
 			<div class="main_3">
 				<div class="main_3_box">
-          <div class="main_2_box_left"
+          <div class="main_box_left"
               data-aos="fade-down"
               data-aos-easing="linear"
               data-aos-duration="1300">
-            <div class="main_2_sub">
-              <div class="main_2_sub_tit">관광정보</div>
-              <div class="main_2_sub_line"></div>
+            <div class="main_sub">
+              <div class="main_sub_tit">관광정보</div>
+              <div class="main_sub_line"></div>
             </div>
             <div class="main_3_sub_con">
               <div class="main_3_sub_txt">
@@ -67,32 +74,32 @@
               </div>
             </div>
             <div class="main_shap">경상도에 이렇게 갈 곳이 많다구요😎</div>
-            <img class="main_3_sub_y" src="/img/yellow.png" alt="색연필" />
+            <img class="main_3_sub_y" src="/img/yellow.png" alt="pencil" />
           </div>
-          <div class="main_2_box_right">
+          <div class="main_box_right">
           </div>
 				</div>
 			</div>
-			<div class="main_2">
-        <div class="main_2_box">
-          <div class="main_2_box_left"
+			<div class="main_4">
+        <div class="main_4_box">
+          <div class="main_box_left"
               data-aos="fade-down"
               data-aos-easing="linear"
               data-aos-duration="1300">
-            <div class="main_2_sub">
-              <div class="main_2_sub_tit">유저광장</div>
-              <div class="main_2_sub_line"></div>
+            <div class="main_sub">
+              <div class="main_sub_tit">유저광장</div>
+              <div class="main_sub_line"></div>
             </div>
-            <div class="main_2_sub_con">
-              <div class="main_2_sub_txt">
+            <div class="main_sub_con">
+              <div class="main_sub_txt">
                 유저들과 다양한<br />
                 재미를 공유해봐요!
               </div>
             </div>
             <div class="main_shap">'가가가가?'같은 경상도식 유머가 있어요🤗</div>
-            <img class="main_sub_y" src="/img/yellow.png" alt="색연필" />
+            <img class="main_sub_y" src="/img/yellow.png" alt="pencil" />
           </div>
-          <div class="main_2_box_right">
+          <div class="main_box_right">
           </div>
 				</div>
 			</div>
--- resources/js/app.js
+++ resources/js/app.js
@@ -8,14 +8,22 @@ import VueCookies from "vue-cookies";
 import router from './router.js';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import 'animate.css';
+// fontawesome import
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+library.add(fas);
 
 const app = createApp({
 	components: {
 		OpenComponent,
 	}
 })
+
 .use(store)
 .use(router)
+.component('font-awesome-icon', FontAwesomeIcon)
 .use(VueCookies);  //쿠키사용
 
 AOS.init();
