012a62cf4e51c0756e582368d3bce20e3f29f7b2
deid01059 <deid01059@gmail.com>
2023-12-27 20:19:13 +0900

    1227오류해결 부트스트랩 제거

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,9 +17,9 @@
 		import { library } from '@fortawesome/fontawesome-svg-core';
 		import { fas } from '@fortawesome/free-solid-svg-icons';
 		import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-		import BootstrapVue3 from 'bootstrap-vue-3';
-		import 'bootstrap/dist/css/bootstrap.css'
-		import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+		// import BootstrapVue3 from 'bootstrap-vue-3';
+		// import 'bootstrap/dist/css/bootstrap.css'
+		// import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 		library.add(fas);
 		const app = createApp({
 			components: {
