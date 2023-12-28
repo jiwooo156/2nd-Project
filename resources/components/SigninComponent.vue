3550b9934d79c90c80588b69b8344724ea632ea1
deid01059 <deid01059@gmail.com>
2023-12-28 19:29:00 +0900

    1228 회원가입 바리데이션 수정완료

--- resources/components/SigninComponent.vue
+++ resources/components/SigninComponent.vue
@@ -17,12 +17,14 @@
 						<span v-show="err_pw3" class="sign_errmsg">숫자를 1글자 이상 작성해 주세요.</span>
 						<span v-show="err_pw4" class="sign_errmsg">특수문자(?~!@#)를 1글자 이상 작성해 주세요.</span>
 						<span v-show="err_pw5" class="sign_errmsg">비밀번호 형식이 올바르지 않습니다.</span>
+						<span v-show="err_pw6" class="sign_errmsg">비밀번호를 확인해 주세요.</span>
 						<span v-show="com_pw" class="sign_commsg">사용 가능한 비밀번호입니다.</span>
 						<input type="password" placeholder="영어, 숫자, 특수문자(!?~@#) 최소 1개 포함 8~20"  v-model="pw" id="signin_pw" autocomplete='off' minlength="8" maxlength="20">
 					</div>
 					<div>
 						<span>비밀번호 확인</span>
 						<span v-show="err_pw_chk" class="sign_errmsg">비밀번호와 일치하지 않습니다.</span>
+						<span v-show="err_pw_chk1" class="sign_errmsg">비밀번호확인을 확인해 주세요.</span>
 						<span v-show="com_pw_chk" class="sign_commsg">비밀번호와 일치합니다.</span>
 						<input type="password" placeholder="비밀번호와 동일" v-model="pw_chk" id="signin_pw_chk" autocomplete='off' minlength="8" maxlength="20">
 					</div>
@@ -31,6 +33,7 @@
 						<span v-show="err_name1" class="sign_errmsg">2 ~ 10글자 사이로 작성해 주세요.</span>
 						<span v-show="err_name2" class="sign_errmsg">한국어만 사용 가능합니다.</span>
 						<span v-show="err_name3" class="sign_errmsg">이름 형식이 올바르지 않습니다.</span>
+						<span v-show="err_name4" class="sign_errmsg">이름을 확인해주세요.</span>
 						<span v-show="com_name" class="sign_commsg">사용 가능한 이름입니다.</span>
 						<input type="text" placeholder="한글 2~10" v-model="name" id="signin_name" autocomplete='off' minlength="2" maxlength="10"
 							@input="koreaname"
@@ -38,9 +41,10 @@
 					</div>
 					<div>
 						<span>닉네임</span>
-						<span v-show="err_nick1" v-if="!(this.com_nick)" class="sign_errmsg">2 ~ 8글자 사이로 작성해 주세요.</span>
+						<span v-show="err_nick1" v-if="!(this.com_nick)" class="sign_errmsg">닉네임은 2~8자리로 작성해 주세요.</span>
 						<span v-show="err_nick2" v-if="!(this.com_nick)" class="sign_errmsg">영어, 숫자, 한글만 사용 가능합니다.</span>
 						<span v-show="err_nick3" v-if="!(this.com_nick)" class="sign_errmsg">닉네임 형식이 올바르지 않습니다.</span>
+						<span v-show="err_nick4" v-if="!(this.com_nick)" class="sign_errmsg">닉네임을 확인해 주세요.</span>
 						<span
 							v-if="$store.state.nickFlg === 1&&this.com_nick" 
 							class="sign_commsg"
@@ -66,6 +70,7 @@
 						<span v-show="err_birthdate1" class="sign_errmsg">-를 제외한 값을 입력해 주세요.</span>
 						<span v-show="err_birthdate2" class="sign_errmsg">생년월일을 8자리로 작성해 주세요.</span>
 						<span v-show="err_birthdate3" class="sign_errmsg">생년월일 형식이 올바르지 않습니다.</span>
+						<span v-show="err_birthdate4" class="sign_errmsg">생년월일을 확인해 주세요.</span>
 						<span v-show="com_birthdate" class="sign_commsg">사용 가능한 생년월일입니다.</span>
 						<input type="text" placeholder="YYYYMMDD"  v-model="birthdate" id="signin_birthdate" autocomplete='off' minlength="8" maxlength="8">
 					</div>
@@ -74,6 +79,7 @@
 						<span v-show="err_phone1" class="sign_errmsg">-를 제외한 값을 입력해 주세요.</span>
 						<span v-show="err_phone2" class="sign_errmsg">휴대폰번호는 11자리로 작성해 주세요.</span>
 						<span v-show="err_phone3" class="sign_errmsg">전화번호 형식이 올바르지 않습니다.</span>
+						<span v-show="err_phone4" class="sign_errmsg">전화번호를 확인해 주세요.</span>
 						<span v-show="com_phone" class="sign_commsg">사용 가능한 전화번호입니다.</span>
 						<input type="text" placeholder="휴대폰번호" v-model="phone" id="signin_phone"  autocomplete='off' minlength="11" maxlength="11">
 					</div>
@@ -86,12 +92,12 @@
 					</div>
 					<div class="sign_footer_box center">
 						<button class="sign_footer_btn pointer"
-							v-if="nickChkFlg"
 							@click="signin"
 						>회원가입</button>
-						<button class="sign_footer_btn pointer"
+						<!-- <button class="sign_footer_btn pointer"
 							v-if="!nickChkFlg"
-						>회원가입</button>
+							@click="signin"
+						>회원가입</button> -->
 						<router-link :to="'/main'" class="sign_footer_btn pointer">취소</router-link>
 					</div>
 				</div>
@@ -119,24 +125,29 @@ export default {
 			err_pw3: false,
 			err_pw4: false,
 			err_pw5: false,
+			err_pw6: false,
 			com_pw: false,
 			err_pw_chk: false,
 			com_pw_chk: false,
 			err_name1: false,
 			err_name2: false,
 			err_name3: false,
+			err_name4: false,
 			com_name: false,
 			err_nick1: false,
 			err_nick2: false,
 			err_nick3: false,
+			err_nick4: false,
 			com_nick: false,
 			err_birthdate1: false,
 			err_birthdate2: false,
 			err_birthdate3: false,
+			err_birthdate4: false,
 			com_birthdate: false,
 			err_phone1: false,
 			err_phone2: false,
 			err_phone3: false,
+			err_phone4: false,
 			com_phone: false,
 			nickChkFlg: false,
 		}
@@ -205,6 +216,7 @@ export default {
 				this.err_pw3 = false;
 				this.err_pw4 = false;
 				this.err_pw5 = false;
+				this.err_pw6 = false;
 				this.com_pw = false;
 				return;
 			}else if(!VAR4.test(this.pw)){
@@ -213,6 +225,7 @@ export default {
 				this.err_pw3 = false;
 				this.err_pw4 = false;
 				this.err_pw5 = false;
+				this.err_pw6 = false;
 				this.com_pw = false;
 				return;
 			}else if(!VAR1.test(this.pw)){
@@ -221,6 +234,7 @@ export default {
 				this.err_pw3 = false;
 				this.err_pw4 = false;
 				this.err_pw5 = false;
+				this.err_pw6 = false;
 				this.com_pw = false;
 				return;
 			}else if(!VAR2.test(this.pw)){
@@ -229,6 +243,7 @@ export default {
 				this.err_pw3 = true;
 				this.err_pw4 = false;
 				this.err_pw5 = false;
+				this.err_pw6 = false;
 				this.com_pw = false;
 				return;
 			}else if(!VAR3.test(this.pw)){
@@ -237,6 +252,7 @@ export default {
 				this.err_pw3 = false;
 				this.err_pw4 = true;
 				this.err_pw5 = false;
+				this.err_pw6 = false;
 				this.com_pw = false;
 				return;
 			}else if(!VAR5.test(this.pw)||!this.pw){
@@ -245,6 +261,7 @@ export default {
 				this.err_pw3 = false;
 				this.err_pw4 = false;
 				this.err_pw5 = true;
+				this.err_pw6 = false;
 				this.com_pw = false;
 				return;
 			}
@@ -253,19 +270,23 @@ export default {
 			this.err_pw3 = false;
 			this.err_pw4 = false;
 			this.err_pw5 = false;
+			this.err_pw6 = false;
 			this.com_pw = true;
 		},
 		pwchkval(){
 			if(this.pw_chk===""){
 				this.err_pw_chk = false;
+				this.err_pw_chk1 = false;
 				this.com_pw_chk = false;
 				return;
 			}else if(this.pw_chk !== this.pw||!this.pw_chk){
 				this.err_pw_chk = true;
+				this.err_pw_chk1 = false;
 				this.com_pw_chk = false;
 				return;
 			}
 			this.err_pw_chk = false;
+			this.err_pw_chk1 = false;
 			this.com_pw_chk = true;
 		},
 		nameval(){
@@ -276,30 +297,35 @@ export default {
 				this.err_name1 = false;
 				this.err_name2 = false;
 				this.err_name3 = false;
+				this.err_name4 = false;
 				this.com_name = false;
 				return;
 			}else if(!VAR.test(this.name)){
 				this.err_name1 = true;
 				this.err_name2 = false;
 				this.err_name3 = false;
+				this.err_name4 = false;
 				this.com_name = false;
 				return;
 			}else if(!VAR1.test(this.name)){
 				this.err_name1 = false;
 				this.err_name2 = true;
 				this.err_name3 = false;
+				this.err_name4 = false;
 				this.com_name = false;
 				return;
 			}else if(!VAR2.test(this.name)||!this.name){
 				this.err_name1 = false;
 				this.err_name2 = false;
 				this.err_name3 = true;
+				this.err_name4 = false;
 				this.com_name = false;
 				return;
 			}
 			this.err_name1 = false;
 			this.err_name2 = false;
 			this.err_name3 = false;
+			this.err_name4 = false;
 			this.com_name = true;
 		},
 		birthdateval(){
@@ -316,30 +342,35 @@ export default {
 				this.err_birthdate1 = false;
 				this.err_birthdate2 = false;
 				this.err_birthdate3 = false;
+				this.err_birthdate4 = false;
 				this.com_birthdate = false;
 				return;
 			}else if(!VAR.test(this.birthdate)){
 				this.err_birthdate1 = true;
 				this.err_birthdate2 = false;
 				this.err_birthdate3 = false;
+				this.err_birthdate4 = false;
 				this.com_birthdate = false;
 				return;
 			}else if(!VAR1.test(this.birthdate)){
 				this.err_birthdate1 = false;
 				this.err_birthdate2 = true;
 				this.err_birthdate3 = false;
+				this.err_birthdate4 = false;
 				this.com_birthdate = false;
 				return;
 			}else if(!VAR2.test(this.birthdate)||!this.birthdate||this.birthdate>=formattedDate){
 				this.err_birthdate1 = false;
 				this.err_birthdate2 = false;
 				this.err_birthdate3 = true;
+				this.err_birthdate4 = false;
 				this.com_birthdate = false;
 				return;
 			}
 			this.err_birthdate1 = false;
 			this.err_birthdate2 = false;
 			this.err_birthdate3 = false;
+			this.err_birthdate4 = false;
 			this.com_birthdate = true;
 		},
 		phoneval(){
@@ -350,30 +381,35 @@ export default {
 				this.err_phone1 = false;
 				this.err_phone2 = false;
 				this.err_phone3 = false;
+				this.err_phone4 = false;
 				this.com_phone = false;
 				return;
 			}else if(!VAR.test(this.phone)){
 				this.err_phone1 = true;
 				this.err_phone2 = false;
 				this.err_phone3 = false;
+				this.err_phone4 = false;
 				this.com_phone = false;
 				return;
 			}else if(!VAR1.test(this.phone)){
 				this.err_phone1 = false;
 				this.err_phone2 = true;
 				this.err_phone3 = false;
+				this.err_phone4 = false;
 				this.com_phone = false;
 				return;
 			}else if(!VAR2.test(this.phone)){
 				this.err_phone1 = false;
 				this.err_phone2 = false;
 				this.err_phone3 = true;
+				this.err_phone4 = false;
 				this.com_phone = false;
 				return;
 			}
 			this.err_phone1 = false;
 			this.err_phone2 = false;
 			this.err_phone3 = false;
+			this.err_phone4 = false;
 			this.com_phone = true;
 		},
 		nickval(){
@@ -384,68 +420,83 @@ export default {
 				this.err_nick1 = false;
 				this.err_nick2 = false;
 				this.err_nick3 = false;
+				this.err_nick4 = false;
 				this.com_nick = false;
 				return;
 			}else if(!VAR.test(this.nick)){
 				this.err_nick1 = true;
 				this.err_nick2 = false;
 				this.err_nick3 = false;
+				this.err_nick4 = false;
 				this.com_nick = false;
 				return;
 			}else if(!VAR1.test(this.nick)){
 				this.err_nick1 = false;
 				this.err_nick2 = true;
 				this.err_nick3 = false;
+				this.err_nick4 = false;
 				this.com_nick = false;
 				return;
 			}else if(!VAR2.test(this.nick)||!this.nick){
 				this.err_nick1 = false;
 				this.err_nick2 = false;
 				this.err_nick3 = true;
+				this.err_nick4 = false;
 				this.com_nick = false;
 				return;
 			}
 			this.err_nick1 = false;
 			this.err_nick2 = false;
 			this.err_nick3 = false;
+			this.err_nick4 = false;
 			this.com_nick = true;
 			this.nick_chk();
 		},
 		signin(){
-			if(this.com_pw&&this.com_pw_chk&&this.com_name&&this.com_birthdate&&this.com_phone&&this.$store.state.nickFlg===1){
+			if(this.com_pw&&this.com_pw_chk&&this.com_name&&this.com_birthdate&&this.com_phone&&this.$store.state.nickFlg===1&&this.nickChkFlg){
 				this.$store.dispatch('actionSignIn');
 			}else{
 				if(!this.com_pw){
-					this.err_pw1 = true;
+					this.err_pw1 = false;
 					this.err_pw2 = false;
 					this.err_pw3 = false;
 					this.err_pw4 = false;
 					this.err_pw5 = false;
+					this.err_pw6 = true;
 					this.com_pw = false;
 				}
 				if(!this.com_pw_chk){
-					this.err_pw_chk = true;
+					this.err_pw_chk = false;
+					this.err_pw_chk1 = true;
 					this.com_pw_chk = false;
 				}
 				if(!this.com_name){
-					this.err_name1 = true;
+					this.err_name1 = false;
 					this.err_name2 = false;
 					this.err_name3 = false;
+					this.err_name4 = true;
 					this.com_name = false;
 				}
 				if(!this.com_birthdate){
 					this.err_birthdate1 = false;
-					this.err_birthdate2 = true;
+					this.err_birthdate2 = false;
 					this.err_birthdate3 = false;
+					this.err_birthdate4 = true;
 					this.com_birthdate = false;
 				}
 				if(!this.com_phone){
 					this.err_phone1 = false;
-					this.err_phone2 = true;
+					this.err_phone2 = false;
 					this.err_phone3 = false;
+					this.err_phone4 = true;
 					this.com_phone = false;
 				}
 				if(!this.com_nick){
+					this.err_nick1 = false;
+					this.err_nick2 = false;
+					this.err_nick3 = false;
+					this.err_nick4 = true;
+					this.com_nick = false;
 					this.nick_chk();
 				}
 			}
